cad9ad00beb9d95e5569d028abddb1c81edd0a11
hitiksaini <hitik9045saini@gmail.com>
2020-06-18 15:06:49 +0530

    Updates

---
+++ precache-manifest.f62da411ba7d44ff8f694cec829e1018.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0eae7f78e87b0bd1c3e47ca5e74d73a",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "224d8770a46566f63e8b",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6be8f8d5f50340c994f",
+    "url": "/react/static/js/2.47c71f1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.47c71f1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224d8770a46566f63e8b",
+    "url": "/react/static/js/main.00964baa.chunk.js"
+  },
+  {
+    "revision": "8554e70fd577e79424c6",
+    "url": "/react/static/js/runtime-main.738e39ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
