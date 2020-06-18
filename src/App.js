8a6de6c8eacb20e1d44bdcfc85a835b05d45a4b9
hitiksaini <hitik9045saini@gmail.com>
2020-06-18 15:14:39 +0530

    few chnages

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You have succesfully deployed your React App.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://hitik20.tech"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hitik
         </a>
       </header>
     </div>
